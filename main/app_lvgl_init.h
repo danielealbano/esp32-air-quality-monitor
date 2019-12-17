a774ba15c2d35bb84068f8f2e1f18b9498e78fcb
Daniele Salvatore Albano <d.albano@gmail.com>
2019-12-17 22:21:53 +0000

    Get rid of the old cpp support in the headers

--- main/app_lvgl_init.h
+++ main/app_lvgl_init.h
@@ -1,10 +1,6 @@
 #ifndef APP_LVGL_INIT_H
 #define APP_LVGL_INIT_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void app_lvgl_init_lv();
 void app_lvgl_init_display_spi();
 void app_lvgl_init_display_hw();
@@ -13,9 +9,5 @@ void app_lvgl_init_display();
 void app_lvgl_init_register_tick_hook();
 void app_lvgl_init_register_task_handler_task();
 void app_lvgl_init();
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
--- main/app_ui.h
+++ main/app_ui.h
@@ -1,16 +1,9 @@
 #ifndef APP_UI_INIT_H
 #define APP_UI_INIT_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void app_ui_build();
-void app_ui_set_co2_value(int co2);
-void app_ui_set_pm25_value(int pm25);
-
-#ifdef __cplusplus
-}
-#endif
+void app_ui_chart_temp_add_point(int temp);
+void app_ui_chart_co2_add_point(int co2);
+void app_ui_chart_pm25_add_point(int pm25);
 
 #endif
