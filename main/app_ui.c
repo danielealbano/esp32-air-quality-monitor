c048737d3b1d8a2b05b41ada7f73a6a64f45be7f
Daniele Salvatore Albano <d.albano@gmail.com>
2019-12-17 22:25:00 +0000

    Update the UI to display 3 charts of the temperature, co2 and pm25 with the related current value using labels on the right

--- main/app_ui.c
+++ main/app_ui.c
@@ -1,115 +1,164 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include "esp_log.h"
 
 #include "lvgl/lvgl.h"
 #include "app_lvgl.h"
 
-static lv_obj_t* meter_co2;
-static lv_obj_t* label_co2_value;
-static lv_obj_t* meter_pm25;
-static lv_obj_t* label_pm25_value;
+#define CHART_SERIES_CO2_MIN 0.0
+#define CHART_SERIES_CO2_MAX 1500.0
+#define CHART_SERIES_TEMP_MIN 0.0
+#define CHART_SERIES_TEMP_MAX 50.0
+#define CHART_SERIES_PM25_MIN 0.0
+#define CHART_SERIES_PM25_MAX 2000.0
 
-static lv_style_t style_lmeter;
+lv_obj_t* chart_co2;
+lv_chart_series_t* chart_series_co2;
+lv_obj_t* chart_value_label_co2;
 
-void app_ui_init_meter_style()
+lv_obj_t* chart_temp;
+lv_chart_series_t* chart_series_temp;
+lv_obj_t* chart_value_label_temp;
+
+lv_obj_t* chart_pm25;
+lv_chart_series_t* chart_series_pm25;
+lv_obj_t* chart_value_label_pm25;
+
+static lv_style_t chart_style;
+static lv_style_t chart_name_label_style;
+static lv_style_t chart_value_label_style;
+
+static const char* TAG = "APP/UI";
+
+#define APP_UI_CONTAINER_CREATE(parent, x, y, width, height) \
+    ({ \
+         \
+        lv_obj_t* __obj = lv_obj_create(parent, NULL); \
+        lv_obj_set_pos(__obj, x, y); \
+        lv_obj_set_size(__obj, width, height); \
+        lv_obj_set_style(__obj, &lv_style_plain); \
+        __obj; \
+    })
+
+void app_ui_init_chart_style()
 {
-    lv_style_copy(&style_lmeter, &lv_style_pretty_color);
-    style_lmeter.line.width = 4;
-    style_lmeter.line.color = LV_COLOR_SILVER;
-    //style_lmeter.body.main_color = lv_color_hex(0x91bfed);
-    style_lmeter.body.grad_color = lv_color_hex(0x04386c);
-    style_lmeter.body.padding.left = 14;
+    lv_style_copy(&chart_style, &lv_style_pretty);
+
+    chart_style.body.radius = 0;
 }
 
-lv_obj_t* app_ui_init_meter(lv_obj_t* scr)
+void app_ui_init_chart_name_label_style()
 {
-    lv_obj_t* lmeter;
-
-    lmeter = lv_lmeter_create(scr, NULL);
-    lv_lmeter_set_range(lmeter, 0, 5000);
-    lv_lmeter_set_value(lmeter, 0);
-    lv_lmeter_set_scale(lmeter, 240, 31);
-    lv_lmeter_set_style(lmeter, LV_LMETER_STYLE_MAIN, &style_lmeter);
-    lv_obj_set_size(lmeter, 130, 130);
-    lv_obj_align(lmeter, NULL, LV_ALIGN_CENTER, 0, 0);
-
-    return lmeter;
+    lv_style_copy(&chart_name_label_style, &lv_style_pretty);
+    chart_name_label_style.text.font = &lv_font_roboto_12;
 }
 
-lv_obj_t* app_ui_init_meter_labels(lv_obj_t* scr, const char* text)
+void app_ui_init_chart_value_label_style()
 {
-    lv_obj_t* label_text = lv_label_create(scr, NULL);
-    lv_label_set_text(label_text, text);
-    lv_obj_align(label_text, NULL, LV_ALIGN_CENTER, 0, -5);
-
-    lv_obj_t* label_value =  lv_label_create(scr, NULL);
-    lv_label_set_text(label_value, "0");
-    lv_obj_align(label_value, NULL, LV_ALIGN_CENTER, 0, 20);
-
-    return label_value;
+    lv_style_copy(&chart_value_label_style, &lv_style_pretty);
+    chart_value_label_style.text.font = &lv_font_roboto_28;
 }
 
-void app_ui_init_co2(lv_obj_t* scr)
+lv_obj_t* app_ui_chart_create(lv_obj_t* container, int min, int max)
 {
-    meter_co2 = app_ui_init_meter(scr);
-    label_co2_value = app_ui_init_meter_labels(scr, "CO2 (ppm)");
+    lv_obj_t* chart = lv_chart_create(container, NULL);
+    lv_obj_set_style(chart, &chart_style);
+    lv_obj_set_size(chart, 230, 80);
+    lv_obj_align(chart, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
+    lv_chart_set_type(chart, LV_CHART_TYPE_LINE);
+    lv_chart_set_update_mode(chart, LV_CHART_UPDATE_MODE_SHIFT);
+    lv_chart_set_point_count(chart, 60);
+    lv_chart_set_series_opa(chart, LV_OPA_70);
+    lv_chart_set_series_width(chart, 4);
+    lv_chart_set_range(chart, min, max);
+
+    return chart;
 }
 
-void app_ui_init_pm25(lv_obj_t* scr)
+lv_chart_series_t* app_ui_chart_series_create(lv_obj_t* chart, lv_color_t color)
 {
-    meter_pm25 = app_ui_init_meter(scr);
-    label_pm25_value = app_ui_init_meter_labels(scr, "PM2.5 (ppm)");
+    return lv_chart_add_series(chart, color);
+}
+
+lv_obj_t* app_ui_chart_labels_create(lv_obj_t* container, char* text)
+{
+    lv_obj_t * chart_name_label = lv_label_create(container, NULL);
+    lv_label_set_align(chart_name_label, LV_LABEL_ALIGN_LEFT);
+    lv_label_set_text(chart_name_label, text);
+    lv_obj_set_width(chart_name_label, 90);
+    lv_obj_align(chart_name_label, NULL, LV_ALIGN_IN_TOP_LEFT, 240, 10);
+    lv_obj_set_style(chart_name_label, &chart_name_label_style);
+
+    lv_obj_t * chart_value_label = lv_label_create(container, NULL);
+    lv_label_set_align(chart_value_label, LV_LABEL_ALIGN_LEFT);
+    lv_label_set_text(chart_value_label, "0");
+    lv_obj_set_width(chart_value_label, 90);
+    lv_obj_align(chart_value_label, NULL, LV_ALIGN_IN_TOP_LEFT, 240, 30);
+    lv_obj_realign(chart_value_label);
+    lv_obj_set_style(chart_value_label, &chart_value_label_style);
+
+    return chart_value_label;
 }
 
 void app_ui_build()
 {
-    app_ui_init_meter_style();
+    app_ui_init_chart_style();
+    app_ui_init_chart_name_label_style();
+    app_ui_init_chart_value_label_style();
 
-    lv_obj_t* scr = lv_disp_get_scr_act(NULL);
+    lv_obj_t* screen = lv_disp_get_scr_act(NULL);
 
-    lv_obj_t* scr_left = lv_obj_create(scr, NULL);
-    lv_obj_set_pos(scr_left, 0, 0);
-    lv_obj_set_size(scr_left, 160, 240);
-    lv_obj_set_style(scr_left, &lv_style_plain);
+    lv_obj_t* container_temp = APP_UI_CONTAINER_CREATE(screen, 0, 0, 320, 80);
+    lv_obj_t* container_co2 = APP_UI_CONTAINER_CREATE(screen, 0, 80, 320, 80);
+    lv_obj_t* container_pm25 = APP_UI_CONTAINER_CREATE(screen, 0, 160, 320, 80);
 
-    lv_obj_t* scr_left_top = lv_obj_create(scr_left, NULL);
-    lv_obj_set_pos(scr_left_top, 0, 0);
-    lv_obj_set_size(scr_left_top, 160, 140);
-    lv_obj_set_style(scr_left_top, &lv_style_plain);
+    chart_value_label_temp = app_ui_chart_labels_create(container_temp, "Temp (c°)");
+    chart_temp = app_ui_chart_create(container_temp, 0, 50);
+    chart_series_temp = app_ui_chart_series_create(chart_temp, LV_COLOR_RED);
 
-    lv_obj_t* scr_left_bottom = lv_obj_create(scr_left, NULL);
-    lv_obj_set_pos(scr_left_bottom, 0, 120);
-    lv_obj_set_size(scr_left_bottom, 160, 140);
-    lv_obj_set_style(scr_left_bottom, &lv_style_plain);
+    chart_value_label_co2 = app_ui_chart_labels_create(container_co2, "CO2 (ppm)");
+    chart_co2 = app_ui_chart_create(container_co2, 0, 1200);
+    chart_series_co2 = app_ui_chart_series_create(chart_co2, LV_COLOR_GREEN);
 
-    app_ui_init_co2(scr_left_top);
-    app_ui_init_pm25(scr_left_bottom);
-
-    lv_obj_t* scr_right = lv_obj_create(scr, NULL);
-    lv_obj_set_pos(scr_right, 160, 0);
-    lv_obj_set_size(scr_right, 160, 240);
-    lv_obj_set_style(scr_right, &lv_style_plain);
+    chart_value_label_pm25 = app_ui_chart_labels_create(container_pm25, "PM2.5 (ppm)");
+    chart_pm25 = app_ui_chart_create(container_pm25, 0, 1200);
+    chart_series_pm25 = app_ui_chart_series_create(chart_pm25, LV_COLOR_BLUE);
 }
 
-void app_ui_update_meter_and_label(int value, lv_obj_t* meter, lv_obj_t* label)
+void app_ui_chart_series_add_point(lv_obj_t * chart, lv_chart_series_t * series, int value)
 {
-    char value_text[10];
-
-    itoa(value, value_text, 10);
-
     app_lvgl_ui_global_lock_grab();
 
-    lv_lmeter_set_value(meter_co2, value);
-    lv_label_set_text(label_co2_value, value_text);
+    lv_chart_set_next(chart, series, value);
     
     app_lvgl_ui_global_lock_release();
 }
 
-void app_ui_set_co2_value(int co2)
+void app_ui_label_value_update(lv_obj_t * label, int value)
 {
-    app_ui_update_meter_and_label(co2, meter_co2, label_co2_value);
+    app_lvgl_ui_global_lock_grab();
+
+    char buf[10];
+    sprintf(buf, "%d", value);
+    lv_label_set_text(label, buf);
+    
+    app_lvgl_ui_global_lock_release();
 }
 
-void app_ui_set_pm25_value(int pm25)
+void app_ui_chart_temp_add_point(int temp)
 {
-    app_ui_update_meter_and_label(pm25, meter_pm25, label_pm25_value);
+    app_ui_chart_series_add_point(chart_temp, chart_series_temp, temp);
+    app_ui_label_value_update(chart_value_label_temp, temp);
+}
+
+void app_ui_chart_co2_add_point(int co2)
+{
+    app_ui_chart_series_add_point(chart_co2, chart_series_co2, co2);
+    app_ui_label_value_update(chart_value_label_co2, co2);
+}
+
+void app_ui_chart_pm25_add_point(int pm25)
+{
+    app_ui_chart_series_add_point(chart_pm25, chart_series_pm25, pm25);
+    app_ui_label_value_update(chart_value_label_pm25, pm25);
 }
